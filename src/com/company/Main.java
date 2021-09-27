8ce8c9cb66034018afd398a1ca9c45013f75b7e6
MH210442 <MH210442@reigate.ac.uk>
2021-09-27 09:50:41 +0100

    jhgjkhg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Random random = new Random();
         Scanner input = new Scanner(System.in);
 
@@ -14,21 +14,10 @@ public class Main {
         System.out.println("Guess a number from 1 to 20");
         int guess = input.nextInt();
         int counter = 1;
-
-        while(guess != randNum){
-            if (guess - randNum == 1 || -1){
-                System.out.println("WARM!!!");
-            }
-            System.out.println("Wrong, try again");
-            guess = input.nextInt();
-            counter ++;
-
-
-        }
-
         if (guess == randNum){
             System.out.println("Correct! It took you " + counter + " tries.");
         }
+
 
     }
 }
