15422cae3eeccf8f3c2528a94af45d17157eaab8
MH210442 <MH210442@reigate.ac.uk>
2021-09-27 10:33:45 +0100

    jhgjkhg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,9 @@ package com.company;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class Main {
 
@@ -11,13 +14,31 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         int randNum = random.nextInt(20) + 1;
+        System.out.println(randNum);
         System.out.println("Guess a number from 1 to 20");
         int guess = input.nextInt();
         int counter = 1;
         while (guess != randNum){
             counter ++;
             System.out.println("Try again:");
+            int diff1 = randNum - guess;
             guess = input.nextInt();
+            int diff2 = randNum - guess;
+            if (guess != randNum) {
+                if (diff1 < 0) {
+                    diff1 = diff1 * -1;
+                }
+                if (diff2 < 0) {
+                    diff2 = diff2 * -1;
+                }
+                if (diff2 < diff1) {
+                    System.out.println("Warmer");
+                } else if (diff2 > diff1) {
+                    System.out.println("Colder");
+                }
+            }
+
+
         }
 
         System.out.println("Correct! It took you " + counter + " tries.");
